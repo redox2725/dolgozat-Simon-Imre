0d3e79aa6780546a7a89fc936f6074a5d2642e85
redox2725 <56677664+redox2725@users.noreply.github.com>
2020-03-26 09:52:33 +0100

    Add files via upload

---
+++ doga Simon Imre 03.26/doga/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace doga
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            double versenyzokszama = 0; 
+            List<versenyzo> vers = new List<versenyzo>();
+            foreach (var i in File.ReadAllLines(@"..\..\res\fob2016.txt"))
+            {
+                vers.Add(new versenyzo(i));
+            }
+
+
+
+
+            versenyzokszama = vers.Count();
+            Console.WriteLine("3. feladat: \nVersenyzők száma: {0}", versenyzokszama);
+
+
+
+            Console.WriteLine("\nFeladat 4");
+            {
+                int no = vers.FindAll(x => x.kategoria.Contains("Noi")).Count;
+                double noiarany = (Convert.ToDouble(no) / vers.Count) * 100;
+                Console.WriteLine($"A női versenyzők aránya: {noiarany.ToString("#.##")}%");
+            }
+
+
+
+
+            StreamWriter sw = new StreamWriter("osszpontFF.txt", false);
+            List<versenyzo> ferfiak = vers.FindAll(x => x.kategoria.Contains("Felnott ferfi"));
+            foreach (versenyzo f in ferfiak)
+            {
+                sw.WriteLine(f.nev + ";" + f.osszpont);
+            }
+            sw.Close();
+
+
+            versenyzo Noilegjobb = vers.FindAll(x => x.kategoria.Contains("Noi")).OrderByDescending(x => x.osszpont).First();
+            Console.WriteLine($"6. feladat:A bajok női versenyzpő \nnev: {Noilegjobb.nev} \negyesület: {Noilegjobb.egyesulet} \nösszpont: {Noilegjobb.pontok}");
+
+
+
+            var list = vers.GroupBy(x => x.kategoria).Select(x => x.ToList()).ToList();
+
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ doga Simon Imre 03.26/doga/versenyzo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doga
+{
+    class versenyzo
+    {
+        public string nev { get; private set; }
+        public string kategoria { get; private set; }
+        public string egyesulet { get; private set; }
+        public byte[] pontok { get; set; }
+
+        public int osszpont;
+
+
+        public versenyzo(string sor)
+        {
+            string[] s = sor.Split(';');
+            nev = s[0]; 
+            kategoria = s[1]; 
+            egyesulet = s[2]; 
+            pontok = new byte[8]; for (int i = 0; i < pontok.Length; i++)
+            {
+                pontok[i] = byte.Parse(s[i + 3]);
+            }
+        }
+        public int osszpontszam {
+            get 
+            { 
+                int osszpont = 0; Array.Sort(pontok);
+                for (int i = 2; i < pontok.Length; i++) { osszpont += pontok[i];
+                } 
+                if (pontok[0] != 0) osszpont += 10; if (pontok[1] != 0) osszpont += 10; return osszpont; 
+            } 
+        
+        }
+
+
+
+        }
+
+
+    
+    }
+
+
